64da274653c75c42951d9e72670366a5a48b8ffe
Sharon <sharonsandeep46655@gmail.com>
2024-05-06 17:28:24 +0530

    base

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,31 @@
-const Navbar = () => {
-	return <div className="bg-black"></div>
-}
+import { useEffect } from "react";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="bg-black flex flex-col justify-center items-center w-full ">
+      {/* <img src="/Thumbnail.png" id="thumbnail" alt="Wall" className="w-96 md:w-auto" /> */}
+      <div className="border-t pt-12 text-gray-100 font-inter w-full flex justify-around items-center -m-[10%] ">
+        <li className=" list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          How-to
+        </li>
+        <li className="list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          Tech
+        </li>
+        <li className=" list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          Business
+        </li>
+        <li className=" list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          Log-in
+        </li>
+        <li className=" list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          News-Letter
+        </li>
+        <li className=" list-none text-xs md:text-base xl:text-lg mx-1 md:mx-4">
+          Join-us
+        </li>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
