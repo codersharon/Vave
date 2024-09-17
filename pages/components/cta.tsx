26cb1823eb3e6e82cf8b9cba23288467fd8c6660
Sharon <sharonsandeep46655@gmail.com>
2024-09-17 14:26:47 +0530

    joinForm_Application_Frontent- Done

--- pages/components/cta.tsx
+++ pages/components/cta.tsx
@@ -1,44 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CTA = () => {
+  const [submited, setSubmited] = useState(false);
+  const [joinForm, setJoinForm] = useState({
+    name: "",
+    email: "",
+    phonenumber: "",
+    DOB: "",
+  });
+  const handleChange = (e) => {
+    setJoinForm({ ...joinForm, [e.target.name]: e.target.value });
+  };
+
+  function uploadForm(name, email, phonenumber, DOB) {
+    let bodyContent = JSON.stringify({
+      name: name,
+      email: email,
+      phonenumber: phonenumber,
+      DOB: DOB,
+    });
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Content-Type": "application/json",
+    };
+    const r = fetch("/api/", {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    });
+  }
+
+  const handleSubmit = () => {
+    uploadForm(
+      joinForm.name,
+      joinForm.email,
+      joinForm.phonenumber,
+      joinForm.DOB
+    );
+    setSubmited(true);
+  };
   return (
     <div className="w-full bg-black text-black flex flex-col items-center justify-center p-2 lg:p-20">
-      <div
-        id="join-us"
-        className="slide-left focus-within-animate-ping flex flex-col items-center justify-center bg-[#C93EB3] p-5 font-medium m-10 w-full md:w-3/5 lg:w-4/12 h-fit"
-      >
-        <h1 className="text-5xl font-[Italiana]">Join-Us</h1>
-        <input
-          type="name"
-          placeholder="name"
-          id="name"
-          className="w-full p-2 bg-gray-200 rounded outline-none my-2"
-        />
-        <input
-          type="email"
-          placeholder="e-mail"
-          id="email"
-          className="w-full p-2 bg-gray-200 rounded outline-none my-2"
-        />
-        <input
-          type="text"
-          placeholder="phone number"
-          id="phone number"
-          className="w-full p-2 bg-gray-200 rounded outline-none my-2"
-        />
-        <input
-          type="date"
-          placeholder="date of birth"
-          id="date of birth"
-          className="w-full p-2 bg-gray-200 rounded outline-none my-2"
-        />
-        <button
-          type="submit"
-          className="bg-yellow-400 hover:shadow-md hover:shadow-yellow-300 shadow-none  rounded p-2"
+      {submited ? (
+        <h1 className="bg-[#C93EB3] p-2 font-bold">
+          Thank You For Applying Form! We will send an email with in a week
+        </h1>
+      ) : (
+        <div
+          id="join-us"
+          className="slide-left focus-within-animate-ping flex flex-col items-center justify-center bg-[#C93EB3] p-5 font-medium m-10 w-full md:w-3/5 lg:w-4/12 h-fit"
         >
-          Submit
-        </button>
-      </div>
+          <h1 className="text-5xl font-[Italiana]">Join-Us</h1>
+          <input
+            type="name"
+            placeholder="name"
+            id="name"
+            onChange={handleChange}
+            className="w-full p-2 bg-gray-200 rounded outline-none my-2"
+          />
+          <input
+            type="email"
+            placeholder="e-mail"
+            id="email"
+            onChange={handleChange}
+            className="w-full p-2 bg-gray-200 rounded outline-none my-2"
+          />
+          <input
+            type="text"
+            placeholder="phonenumber"
+            id="phonenumber"
+            className="w-full p-2 bg-gray-200 rounded outline-none my-2"
+          />
+          <input
+            type="date"
+            placeholder="date of birth"
+            id="DOB"
+            onChange={handleChange}
+            className="w-full p-2 bg-gray-200 rounded outline-none my-2"
+          />
+          <button
+            type="submit"
+            className="bg-yellow-400 hover:shadow-md hover:shadow-yellow-300 shadow-none  rounded p-2"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      )}
       <div
         id="news-letter"
         className="slide-right focus-within-animate-ping flex flex-col items-center justify-center bg-[#C93EB3] p-5 font-medium m-10 w-full md:w-3/5 lg:w-4/12  h-fit"
--- pages/components/hero.tsx
+++ pages/components/hero.tsx
@@ -3,30 +3,27 @@ import Topic from "./topic";
 
 const Hero = (props) => {
   const [topics, setTopics] = useState(props ? props.topics : []);
-  const topic1 = topics[0];
-  const topic2 = topics[1];
-  const topic3 = topics[2];
   const [postImg1, setPostImg1] = useState(
-    topic1.image ? topic1.image : "/Post_Background.jpg"
-  );
+    topics[0].image ? topics[0].image : "/Post_Background.jpg");
   const [postImg2, setPostImg2] = useState(
-    topic2.image ? topic2.image : "/Post_Background.jpg"
-  );
+    topics[1] ? topics[1].image : "/Post_Background.jpg");
   const [postImg3, setPostImg3] = useState(
-    topic3.image ? topic3.image : "/Post_Background.jpg"
-  );
-  if (postImg1 == "") {
-    setPostImg1("/Post_Background.jpg");
-  }
-  if (postImg2 == "") {
-    setPostImg2("/Post_Background.jpg");
-  }
-  if (postImg3 == "") {
-    setPostImg3("/Post_Background.jpg");
-  }
-  return (
+    topics[2] ? topics[2].image : "/Post_Background.jpg");
+  if (topics[2] != null) {
+    const topic1 = topics[0];
+    const topic2 = topics[1];
+    const topic3 = topics[2];
+    if (postImg1 == "" || postImg1 == null) {
+      setPostImg1("/Post_Background.jpg");
+    }
+    if (postImg2 == "" || postImg2 == null) {
+      setPostImg2("/Post_Background.jpg");
+    }
+    if (postImg3 == "" || postImg3 == null) {
+      setPostImg3("/Post_Background.jpg");
+    }
+    return 
     <div className="border-b border-white mt-12 pb-10 w-full min-[1920px]:w-3/5 flex flex-col items-center justify-center">
-      {topics ? (
         <div className="w-full flex flex-col md:flex-row items-center justify-center md:justify-around">
           <Topic
             id={topic1._id ? topic1._id : ""}
@@ -53,9 +50,25 @@ const Hero = (props) => {
             key={topic3._id}
           />
         </div>
-      ) : null}
     </div>
-  );
-};
+  } else {
+    const topic1 = topics[0];
+    if (postImg1 == "" || postImg1 == null) {
+      setPostImg1("/Post_Background.jpg");
+    }
+      return (
+        <div className="border-b border-white mt-12 pb-10 w-full min-[1920px]:w-3/5 flex flex-col items-center justify-center">
+         <div className="w-full flex flex-col md:flex-row items-center justify-center md:justify-around">
+          <Topic
+            id={topic1._id ? topic1._id : ""}
+            title={topic1.title.slice(0, 32) + "..."}
+            image={postImg1}
+            content={topic1.content}
+            auhtor={"Sharon"}
+            key={topic1._id}
+          />
+        </div>
+        </div>)
+}}
 
 export default Hero;
